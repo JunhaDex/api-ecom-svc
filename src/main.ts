2c923d285841cb72a9a2638d6ab053495c9dd35d
JunhaDex <dk.junha.dev@gmail.com>
2024-12-08 00:19:50 +0900

    setup fastify & connect db

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import multipart from '@fastify/multipart';
+import userAgent from 'fastify-user-agent';
+
+import * as dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+    {
+      bodyParser: true,
+    },
+  );
+  await app.register(userAgent);
+  await app.register(multipart);
+  app.setGlobalPrefix('api/v1');
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  Logger.log(`Server running on port ${process.env.PORT ?? 3000}`);
+  Logger.log(`Timezone: ${process.env.TZ}`);
+  Logger.log(`Server Started: ${new Date().toString()}`);
 }
+
 bootstrap();
---
+++ storage/database/mysql/init/setup.sql
@@ -0,0 +1,237 @@
+create if not exists database service;
+use service;
+
+create table status_user
+(
+    id          int unsigned auto_increment
+        primary key,
+    status_name varchar(255)                        not null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null
+);
+
+INSERT INTO service.status_user (id, status_name, created_at) VALUES (1, 'default', '2024-12-07 16:27:30');
+INSERT INTO service.status_user (id, status_name, created_at) VALUES (2, 'readonly', '2024-12-07 16:27:30');
+INSERT INTO service.status_user (id, status_name, created_at) VALUES (3, 'blocked', '2024-12-07 16:27:30');
+
+
+create table status_product
+(
+    id          int unsigned auto_increment
+        primary key,
+    status_name varchar(255)                        not null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null
+);
+
+INSERT INTO service.status_product (id, status_name, created_at) VALUES (1, 'on sale', '2024-12-07 16:27:30');
+INSERT INTO service.status_product (id, status_name, created_at) VALUES (2, 'hide', '2024-12-07 16:27:30');
+
+create table status_transaction
+(
+    id          int unsigned auto_increment
+        primary key,
+    status_name varchar(255)                        not null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null
+);
+
+INSERT INTO service.status_transaction (id, status_name, created_at) VALUES (1, 'purchased', '2024-12-07 16:52:52');
+INSERT INTO service.status_transaction (id, status_name, created_at) VALUES (2, 'shipped', '2024-12-07 16:52:52');
+INSERT INTO service.status_transaction (id, status_name, created_at) VALUES (3, 'delivered', '2024-12-07 16:52:52');
+INSERT INTO service.status_transaction (id, status_name, created_at) VALUES (4, 'returned', '2024-12-07 16:52:52');
+INSERT INTO service.status_transaction (id, status_name, created_at) VALUES (5, 'partial returned', '2024-12-07 16:52:52');
+INSERT INTO service.status_transaction (id, status_name, created_at) VALUES (6, 'canceled', '2024-12-07 16:52:52');
+
+create table status_shipment
+(
+    id          int unsigned auto_increment
+        primary key,
+    status_name varchar(255)                        not null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null
+);
+
+INSERT INTO service.status_shipment (id, status_name, created_at) VALUES (1, 'in transit', '2024-12-07 22:56:25');
+INSERT INTO service.status_shipment (id, status_name, created_at) VALUES (2, 'delivered', '2024-12-07 22:56:25');
+INSERT INTO service.status_shipment (id, status_name, created_at) VALUES (3, 'error', '2024-12-07 22:56:25');
+
+-- service tables
+create table admin
+(
+    id         int unsigned auto_increment
+        primary key,
+    admin_id   varchar(255)                        not null,
+    pwd        varchar(255)                        not null comment 'password',
+    name       varchar(255)                        not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    constraint admin_id_unique
+        unique (admin_id)
+);
+
+create table user_group
+(
+    id          int unsigned auto_increment
+        primary key,
+    group_name  varchar(100)                        not null,
+    description varchar(255)                        null,
+    created_at  timestamp default CURRENT_TIMESTAMP not null
+);
+
+create table user
+(
+    id             int unsigned auto_increment
+        primary key,
+    user_id        varchar(255)                           not null,
+    pwd            varchar(255)                           not null comment 'password',
+    branch_name    varchar(255)                           not null,
+    branch_manager varchar(255)                           not null,
+    branch_contact varchar(20)                            not null,
+    group_id       int unsigned                           not null,
+    status         int unsigned default '1'               not null,
+    created_at     timestamp    default CURRENT_TIMESTAMP not null,
+    updated_at     datetime     default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    constraint user_id_unique
+        unique (user_id),
+    constraint user_status_user_id_fk
+        foreign key (status) references status_user (id),
+    constraint user_user_group_id_fk
+        foreign key (group_id) references user_group (id)
+);
+
+create table product
+(
+    id            int unsigned auto_increment
+        primary key,
+    product_name  varchar(255)                           not null,
+    description   varchar(255) default ''                null,
+    image_urls    json                                   null,
+    product_price decimal(10, 2)                         not null,
+    status        int unsigned default '1'               not null,
+    created_at    timestamp    default CURRENT_TIMESTAMP not null,
+    constraint product_status_product_id_fk
+        foreign key (status) references status_product (id)
+);
+
+create table user_group_product
+(
+    id         int unsigned auto_increment
+        primary key,
+    group_id   int unsigned                        not null,
+    product_id int unsigned                        not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    constraint user_group_product_unique
+        unique (group_id, product_id),
+    constraint user_group_product_product_id_fk
+        foreign key (product_id) references product (id),
+    constraint user_group_product_user_group_id_fk
+        foreign key (group_id) references user_group (id)
+);
+
+create table cart_user_product
+(
+    id         int unsigned auto_increment
+        primary key,
+    user_id    int unsigned                        not null,
+    product_id int unsigned                        not null,
+    count      int                                 not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    constraint cart_user_product_product_id_fk
+        foreign key (product_id) references product (id),
+    constraint cart_user_product_user_id_fk
+        foreign key (user_id) references user (id)
+);
+
+create table payment
+(
+    id             int unsigned auto_increment
+        primary key,
+    session_key    varchar(255)                        not null comment 'uuidv4',
+    pay_method     varchar(255)                        null,
+    payment_key    varchar(255)                        null comment 'from toss',
+    order_id       varchar(255)                        null comment 'from toss',
+    paid_amount    decimal(10, 2)                      null comment 'from toss',
+    balance_amount decimal(10, 2)                      null comment 'when canceled',
+    paid_at        datetime                            null,
+    created_at     timestamp default CURRENT_TIMESTAMP not null,
+    updated_at     datetime  default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    constraint payment_session_unique
+        unique (session_key)
+);
+
+create table transaction
+(
+    id         int unsigned auto_increment
+        primary key,
+    payment_id int unsigned                           not null,
+    tx_name    varchar(255)                           not null,
+    tx_note    text                                   null,
+    user_id    int unsigned                           not null comment 'user ref key',
+    status     int unsigned default '1'               not null,
+    created_at timestamp    default CURRENT_TIMESTAMP not null,
+    updated_at datetime     default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    constraint transaction_payment_id_fk
+        foreign key (payment_id) references payment (id),
+    constraint transaction_status_transaction_id_fk
+        foreign key (status) references status_transaction (id),
+    constraint transaction_user_id_fk
+        foreign key (user_id) references user (id)
+);
+
+create table transaction_product
+(
+    id         int unsigned auto_increment
+        primary key,
+    tx_id      int unsigned                           not null,
+    product_id int unsigned                           not null,
+    count      int                                    not null,
+    price      decimal(10, 2)                         not null,
+    status     int unsigned default '1'               not null,
+    created_at timestamp    default CURRENT_TIMESTAMP not null,
+    constraint transaction_product_product_id_fk
+        foreign key (product_id) references product (id),
+    constraint transaction_product_status_transaction_id_fk
+        foreign key (status) references status_transaction (id),
+    constraint transaction_product_transaction_id_fk
+        foreign key (tx_id) references transaction (id)
+);
+
+create table courier
+(
+    id           int unsigned auto_increment
+        primary key,
+    courier_name varchar(255)                        null,
+    api_url      varchar(255)                        null,
+    created_at   timestamp default CURRENT_TIMESTAMP not null
+);
+
+create table shipment
+(
+    id              int unsigned auto_increment
+        primary key,
+    tx_id           int unsigned                        not null,
+    courier_id      int unsigned                        not null,
+    address         varchar(255)                        not null,
+    recipient_name  varchar(255)                        not null,
+    recipient_phone varchar(20)                         not null,
+    tracking_no     int                                 not null,
+    status          int unsigned                        not null,
+    created_at      timestamp default CURRENT_TIMESTAMP not null,
+    updated_at      datetime  default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    constraint shipment_courier_id_fk
+        foreign key (courier_id) references courier (id),
+    constraint shipment_status_shipment_id_fk
+        foreign key (status) references status_shipment (id),
+    constraint shipment_transaction_id_fk
+        foreign key (tx_id) references transaction (id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
